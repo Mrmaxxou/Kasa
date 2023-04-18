a27cf73c8507b5d370d92f0c435128748fc27387
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-18 18:17:55 +0200

    Correction accès Home dans index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from'react-router-dom';
 
-import Home from './components/Home';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
