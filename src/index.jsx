6d72de25df9ba94eaba2cdec7b2d580610263238
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-18 18:16:59 +0200

    Supression fichier inutile et initialisation react

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from'react-router-dom';
+
+import Home from './components/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Home />
+    </Router>
+  </React.StrictMode>
+);
