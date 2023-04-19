fe372b350b9d5642df82531a6f033311d3bed488
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-19 17:15:16 +0200

    Ajout hoover sur les cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,42 +6,50 @@ import colors from '../../utils/style/colors'
 // ----------------------------------------------------------------- //
 
 const StyledBlockCard = styled.div`
-position: relative;
-margin: 25px 0px 25px 0px;
+    position: relative;
+    width: 340px;
+    height: 340px;
+    border-radius: 10px;
+    margin: 25px 0px 25px 0px;
+    &:hover {
+        cursor: pointer;
+        box-shadow: 2px 2px 10px ${colors.tertiary};
+    }
+    @media (max-width: 768px){
+        width: 335px;
+        height: 255px;
+    } 
 `
 
 const StyledBackgroundCard = styled.div`
-position: absolute;
-bottom: 0;
-left: 0;
-background-color: rgba(0, 0, 0, 0.2);
-height: 100%;
-width: 100%;
-z-index: 1;
-border-radius: 10px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+    border-radius: 10px;
 `
 
 const StyledTitle = styled.h2`
-position: absolute;
-bottom: 15px;
-margin: 0px 20px 0px 10px;
-position: absolute;
-color: ${colors.secondary};}
-font-size: 18px;
-font-weight: 500;
-z-index: 1;
+    position: absolute;
+    bottom: 15px;
+    margin: 0px 20px 0px 10px;
+    position: absolute;
+    color: ${colors.secondary};}
+    font-size: 18px;
+    font-weight: 500;
+    z-index: 1;
 `
 
 const Styledcover = styled.img`
-width: 340px;
-height: 340px;
-position: relative;
-border-radius: 10px;
-object-fit: cover;
-@media (max-width: 768px){
-    width: 335px;
-    height: 255px;
-  } 
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    object-fit: cover;
+    
+    
 `
 
 // ----------------------------------------------------------------- //
@@ -50,8 +58,7 @@ function Card({title, cover}){
     return (
         <StyledBlockCard>
             <StyledBackgroundCard></StyledBackgroundCard>
-            <StyledTitle>{title}</StyledTitle>
-            
+            <StyledTitle>{title}</StyledTitle>    
             <Styledcover src={cover} alt={title}/>
         </StyledBlockCard>
     )
