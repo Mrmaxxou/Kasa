0ecafb6aa2aeef441dafbe6bfe811563549d606e
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-20 11:40:07 +0200

    Intégration Banner dans Pages About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,24 @@
+import {BannerAbout }from '../../components/Banner'
+
+import styled from 'styled-components'
+
+const StyledAbout = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 70px 100px 30px 100px;
+  @media (max-width: 768px){
+    margin: 16px 20px 22px 20px;
+  } 
+
+`
+
+
 function About(){
     return (
-        <h1>A Propos</h1>
+        <StyledAbout>
+            <BannerAbout/>
+        </StyledAbout>
     )
 
 }
