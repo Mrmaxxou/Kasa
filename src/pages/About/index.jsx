495b46d84665670705de857b117dc1f6c231382b
Mr_m@xou <101470797+Mrmaxxou@users.noreply.github.com>
2023-04-21 15:37:36 +0200

    Création de la page A propos

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,23 +1,33 @@
-import {BannerAbout }from '../../components/Banner'
+import Collapse from '../../components/Collapse'
+import Banner from '../../components/Banner'
+import BannerAbout from '../../assets/banners-propos.jpg'
+import dataAbout from '../../Datas/About.json'
 
 import styled from 'styled-components'
 
 const StyledAbout = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+ align-items: center;
   margin: 70px 100px 30px 100px;
   @media (max-width: 768px){
-    margin: 16px 20px 22px 20px;
+    margin: 16px 20px 140px 20px;
   } 
-
 `
+
 
 
 function About(){
     return (
         <StyledAbout>
-            <BannerAbout/>
+            <Banner backgroundImg={BannerAbout}  height={"223px"}/>
+            {dataAbout.map((data) => (
+            <Collapse 
+              key={data.title}
+              title={data.title}
+              text= {data.text}
+            />
+            ))}
         </StyledAbout>
     )
 
