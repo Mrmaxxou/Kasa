e281dd9136c95635d6b89a418d982f47e9a7fe8f
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-01 19:37:06 +0200

    Modification de card.jsx et création de card.css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,78 +1,22 @@
-import styled from 'styled-components'
-import colors from '../../utils/colors'
+import '../../utils/style/card.css';
 
 import DataLogements from '../../Datas/logements.json'
 import {Link} from 'react-router-dom'
 
-// CSS Header //
-// ----------------------------------------------------------------- //
-
-const StyledBlock = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    width: 100%;
-`
-const StyledBlockCard = styled.div`
-    position: relative;
-    width: 340px;
-    height: 340px;
-    border-radius: 10px;
-    margin: 25px 0px 25px 0px;
-    &:hover {
-        cursor: pointer;
-        box-shadow: 2px 2px 10px ${colors.tertiary};
-    }
-    @media (max-width: 768px){
-        width: 335px;
-        height: 255px;
-        margin: 10px 0px 10px 0px;
-    } 
-`
-const StyledBackgroundCard = styled.div`
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    height: 100%;
-    width: 100%;
-    z-index: 1;
-    border-radius: 10px;
-`
-const StyledTitle = styled.h2`
-    position: absolute;
-    bottom: 15px;
-    margin: 0px 20px 0px 10px;
-    position: absolute;
-    color: ${colors.secondary};}
-    font-size: 18px;
-    font-weight: 500;
-    z-index: 1;
-`
-const Styledcover = styled.img`
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    object-fit: cover;
-    
-    
-`
-
-// ----------------------------------------------------------------- //
 
 function Card() {
     return (
-        <StyledBlock>
+        <section className="container-allcard">
             {DataLogements.map((data) => (
-                <StyledBlockCard key={data.id}>
+                <div className="container-card" key={data.id}>
                     <Link to={{pathname: "/logement/", search: "?id=" + data.id}}>
-                        <StyledBackgroundCard></StyledBackgroundCard>
-                        <StyledTitle>{data.title}</StyledTitle>
-                        <Styledcover src={data.cover} alt={data.title} />
+                        <div className="card-background"></div>
+                        <h2 className="card-title">{data.title}</h2>
+                        <img className="card-img" src={data.cover} alt={data.title} />
                     </Link>
-                </StyledBlockCard>
+                </div>
             ))}
-        </StyledBlock>
+        </section>
     );
 }
 
