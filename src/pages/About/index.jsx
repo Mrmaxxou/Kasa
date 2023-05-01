f59b953adf40561dd19905cc4d94be9c96507e31
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-01 19:43:11 +0200

    Modifcation de about.jsx et création de about.css

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,34 +1,26 @@
 import Collapse from '../../components/Collapse'
 import Banner from '../../components/Banner'
-import BannerAbout from '../../assets/banners-propos.jpg'
+
 import dataAbout from '../../Datas/About.json'
 
-import styled from 'styled-components'
 
-const StyledAbout = styled.section`
-  display: flex;
-  flex-direction: column;
- align-items: center;
-  margin: 70px 100px 30px 100px;
-  @media (max-width: 768px){
-    margin: 16px 20px 140px 20px;
-  } 
-`
-
+import '../../utils/style/banner.css'
+import '../../utils/style/collapse.css'
+import '../../utils/style/about.css'
 
 
 function About(){
     return (
-        <StyledAbout>
-            <Banner backgroundImg={BannerAbout}  height={"223px"}/>
+        <section className="container-about">
+            <Banner bannerClass="container-banner-about"/>
             {dataAbout.map((data) => (
-            <Collapse 
+            <Collapse collapseClass="container-collapse-about"
               key={data.title}
               title={data.title}
               text= {data.text}
             />
             ))}
-        </StyledAbout>
+        </section>
     )
 
 }
