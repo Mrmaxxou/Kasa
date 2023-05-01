c8da6843f36a83986ae8d93897c481d5a8fb9ee9
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-01 19:45:56 +0200

    Suppression des fichier colors.js et Globalstyle.jsx

--- src/utils/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-    primary: '#FF6060',
-    secondary: '#FFFFFF',
-    tertiary: '#000000',
-    backgroundCard: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%)',
-    backgroundBlockCard: '#F7F7F7'
-}
-
-export default colors
