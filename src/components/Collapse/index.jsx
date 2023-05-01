b7041e6c31beaa564df3e55417f05f078a933c46
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-01 19:38:49 +0200

    Modification de collapse.jsx et création de collapse.css

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,119 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 
+import '../../utils/style/collapse.css';
 
-import styled from 'styled-components'
 import IconCollapse from '../../assets/arrow_back_ios.svg'
 
-import colors from '../../utils/colors'
 
-// CSS Collapse //
 
-// ----------------------------------------------------------------- //
-
-const StyledBlockAbout = styled.section`
-    display: flex;
-    flex-direction: column;
-    width: 85%;
-    margin: 0px 0px 0px 0px;
-    @media (max-width: 768px){
-        margin: 20px 0px 0px 0px;
-        width: 100%;
-      } 
-`
-const StyledCollapseTitled = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 5px 0px 5px 0px;
-    background-color: ${colors.primary};
-    border-radius: 5px;
-`
-
-const StyledTexth3 = styled.h3`
-    font-size: 24px;
-    font-weight: 500;
-    color: ${colors.secondary};
-    margin: 0px 0px 0px 18px;
-    @media (max-width: 768px){
-        font-size: 13px;
-      } 
-
-`
-
-const Styledimg = styled.img`
-    margin: 0px 17px 0px 0px;
-    width: 1.1em;
-    filter: invert(87%) sepia(99%) saturate(0%) hue-rotate(183deg) brightness(200%) contrast(97%);
-    rotate: 180deg;
-    cursor: pointer;
-    transform: ${({collapsed}) => (collapsed? 'rotate(180deg)' : 'rotate(0deg)')};
-    transition: transform 0,2s ease-in-out;
-`
-
-const StyledBackgroundText = styled.div`
-    background-color: ${colors.backgroundBlockCard};
-    margin: 0px 0px 31px 0px;
-    border-radius: 5px;
-    overflow: hidden;
-    max-height: ${({collapsed}) => (collapsed? '0px' : '1000px')};
-    transition: max-height 0,6s ease-in-out;
-    @media (max-width: 768px){
-        margin: 0px 0px 0px 0px;
-      } 
-`
-
-const StyledText = styled.p`
-    color: ${colors.primary};
-    font-size: 24px;
-    font-weight: 400;
-    margin: 0px 18px 0px 18px;
-    padding: 36px 0px 20px 0px;
-    @media (max-width: 768px){
-        font-size: 12px;
-        line-height: 17px;
-        padding: 17px 0px 39px 0px;
-      } 
-
-`
-
-// ----------------------------------------------------------------- //
-
-function Collapse({title, text}){
-    const [collapsed, setCollapsed] = useState(true);
-
-    const handleClick = () => {
-        setCollapsed(!collapsed);
-    };
-
-    useEffect(() => {
-        const handleOutsideClick = (event) => {
-            if (event.target.tagName === 'img') {
-                setCollapsed(true);
-            }
-        };
-        document.addEventListener('click', handleOutsideClick);
-        return () => {
-            document.removeEventListener('click', handleOutsideClick);
-        }
-    }, []);
-
+function Collapse({ title, text, collapseClass }) {
+    const [collapsed, setCollapsed] = useState(false);
+  
+    function toggleCollapse() {
+      setCollapsed(!collapsed);
+    }
+  
     return (
-        <StyledBlockAbout>
-            <StyledCollapseTitled>
-                <StyledTexth3>{title}</StyledTexth3>
-                <Styledimg 
-                    src={IconCollapse} 
-                    alt="Icone déplit-replis" 
-                    onClick={handleClick}
-                    collapsed={collapsed}
-                />
-            </StyledCollapseTitled>
-            <StyledBackgroundText collapsed={collapsed}>
-                <StyledText>{text}</StyledText>
-            </StyledBackgroundText>
-        </StyledBlockAbout>
+      <section className={collapseClass}>
+        <div className="collapse-icontitle">
+          <h3 className="collapse-title">{title}</h3>
+          <img
+            className={`collapse-icons ${collapsed ? 'collapsed' : ''}`}
+            src={IconCollapse}
+            alt="Icone déplit-replis"
+            onClick={toggleCollapse}
+          />
+        </div>
+        <div className={`collapse-backgroundtext collapse__content ${collapsed ? 'collapse__content--expanded' : ''}`}>
+          <div className="collapse-text">{text}</div>
+        </div>
+      </section>
     );
-}
+  }
 
 export default Collapse;
