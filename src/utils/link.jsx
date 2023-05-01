a3582d6b31e122f1ce38085835c55bf446a633d5
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-01 19:46:37 +0200

    Modification fichier link.jsx

--- src/utils/link.jsx
+++ src/utils/link.jsx
@@ -1,17 +1,18 @@
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
-import colors from './colors'
+
+import './style/index.css'
 
 export const StyledLink = styled(Link)`
     padding-left: 57px;
-    color: ${colors.primary};
+    color: var(--primary);
     text-decoration: none;
     font-size: 24px;
     font-weight: 500;
     text-align: center;
     &:hover {
         text-decoration: underline;
-        color: ${colors.primary};
+        color: var(--primary);
     }
     @media (max-width: 768px){
         font-size: 12px;
@@ -22,14 +23,14 @@ export const StyledLink = styled(Link)`
 
 export const StyledLink404 = styled(Link)`
     margin: 182px 0px 160px 0px;
-    color: ${colors.primary};
+    color: var(--primary);
     text-decoration: none;
     font-size: 18px;
     font-weight: 500;
     text-align: center;
     &:hover {
         text-decoration: underline;
-        color: ${colors.primary};
+        color: var(--primary);
     }
     @media (max-width: 768px){
         font-size: 14px;
