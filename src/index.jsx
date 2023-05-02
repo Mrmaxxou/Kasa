40e52db97a09b24f5c569c675b68961acbb6bc75
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-02 12:12:54 +0200

    Ajout commentaire dans le fichier index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
+// Importation de React 
 import React from 'react';
+// Importationde de createRoot de ReactDOM
 import { createRoot } from 'react-dom/client'
 
+// Importation de l'ensemble App 
 import App from './App';
+
+// Importation du fichier style css
 import './utils/style/index.css'
 
+// Création de la racine de l'application React 
 createRoot(document.getElementById('root')).render(
 <React.StrictMode>
     <App />
