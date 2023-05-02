40fc414174add6355051b9f79dff91bc19820cc3
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-02 11:05:18 +0200

    Ajout commentaire dans Car.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,16 @@
+// Import du fichier style css
 import '../../utils/style/card.css';
 
+// Import du fichier json contenant les données logement
 import DataLogements from '../../Datas/logements.json'
+
+// Import du composant Link du module react-router-dom
 import {Link} from 'react-router-dom'
 
-
+// Création d'un composant React "Card" 
 function Card() {
+    // Utilisation de la méthode map pour parcourir le tableau DataLogements et créer un élément div pour chaque éléments //
+    // Création d'un link qui ammène à la page logement en incluant l'id de la donnée en tant que paramètre de recherche //
     return (
         <section className="container-allcard">
             {DataLogements.map((data) => (
@@ -20,4 +26,5 @@ function Card() {
     );
 }
 
+// Export de la fontion Banner
 export default Card;
