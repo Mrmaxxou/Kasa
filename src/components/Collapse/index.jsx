8daf375a082352b936c540d6cff7ccfcbf8a4046
Mrmaxxou <user_rougeyronm@outlook.fr>
2023-05-02 11:24:08 +0200

    Ajout comentaire dans Collapse.jsx

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,18 +1,21 @@
+// Import du hook useState de React 
 import React, {useState} from "react";
 
+// Import du fichier style css et de l'icone arrow
 import '../../utils/style/collapse.css';
-
 import IconCollapse from '../../assets/arrow_back_ios.svg'
 
-
-
+// Création d'un composant React "Collapse" 
 function Collapse({ title, text, collapseClass }) {
+  // Initilisation de l'état "collapse"
     const [collapsed, setCollapsed] = useState(false);
-  
+
+  // Fonction pour changer l'état de "collapse" lors du clique sur l'îcone
     function toggleCollapse() {
       setCollapsed(!collapsed);
     }
-  
+    // Retourn de l'élément carousel //
+    // Avec l'icône pour déplier/replier & le contenu de la collapse qui peut être déplié ou replié
     return (
       <section className={collapseClass}>
         <div className="collapse-icontitle">
@@ -31,4 +34,5 @@ function Collapse({ title, text, collapseClass }) {
     );
   }
 
+// Export de la fontion Collapse
 export default Collapse;
